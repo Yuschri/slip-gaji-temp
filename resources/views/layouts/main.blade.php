12142fd0d8ab4375d63a4f2b3a0d4ee4c03c1348
yMisbah <83804390+yMisbah@users.noreply.github.com>
2026-04-06 09:07:42 +0700

    fix: update env

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
-    <title>Slip Gaji</title>
+    <title>DNY - Slip Gaji</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicon_io/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon_io/favicon-32x32.png') }}">
@@ -149,7 +149,7 @@
         </div>
         <ul class="nav flex-column">
             <li class="px-4 py-2"><small class="nav-text">Main</small></li>
-            <!-- <li><a class="nav-link active" href="index.html"><i class="ti ti-home"></i><span
+            <li><a class="nav-link active" href="index.html"><i class="ti ti-home"></i><span
                         class="nav-text">Dashboard</span></a></li>
             <li><a class="nav-link" href="inventory.html"><i class="ti ti-box-seam"></i><span
                         class="nav-text">Inventory</span></a></li>
@@ -157,24 +157,24 @@
                         Product</span></a></li>
             <li><a class="nav-link" href="reports.html"><i class="ti ti-receipt"></i><span
                         class="nav-text">Reports</span></a>
-            </li> -->
+            </li>
             <li><a class="nav-link {{ request()->routeIs('slip-gaji.*') ? 'active' : '' }}"
                     href="{{ route('slip-gaji.index') }}"><i class="ti ti-file-description"></i><span
                         class="nav-text">Slip Gaji</span></a>
             </li>
-            <!-- <li><a class="nav-link" href="404-error.html"><i class="ti ti-alert-circle"></i><span class="nav-text">404
+            <li><a class="nav-link" href="404-error.html"><i class="ti ti-alert-circle"></i><span class="nav-text">404
                         Error</span></a>
             </li>
             <li><a class="nav-link" href="docs.html"><i class="ti ti-file-text"></i><span
                         class="nav-text">Docs</span></a></li>
- -->
 
-            <!-- <li class="px-4 pt-4 pb-2"><small class="nav-text">Account</small></li>
+
+            <li class="px-4 pt-4 pb-2"><small class="nav-text">Account</small></li>
             <li><a class="nav-link" href="signin.html"><i class="ti ti-logout"></i><span class="nav-text">Log
                         in</span></a>
             </li>
             <li><a class="nav-link" href="signup.html"><i class="ti ti-user-plus"></i><span class="nav-text">Sign
-                        up</span></a></li> -->
+                        up</span></a></li>
         </ul>
 
     </aside>
